47a89d9a633bb2349eadf15d64017aebb99c1299
lintkey <lintkey@outlook.com>
2023-06-12 12:41:23 +0800

    🐞 fix:去除网卡识别，更改检测请求地址为1.1.1.1

--- njupt_login.sh
+++ njupt_login.sh
@@ -29,46 +29,35 @@ if [ -n $ISP ]; then
   ISP="@$ISP"
 fi
 
-# 自动选择网卡
-# 在此可配置需要过滤的项(例如过滤cfw的utun网卡：grep -v utun)
-con_status=`ip -f inet a|grep inet|grep -v "127.0.0.1"|grep -v utun`
-# 以最后一个有ipv4连接的网卡作为网卡(我的有线网卡外置，记录靠后，这样可优先使用有线网卡)
-interface=`echo ${con_status}|awk 'END{print $NF}'`
-
-# 未登陆情况下
-#   使用utun时，DNS解析失败，获得空串
-#   不使用utun时，过滤后获得空串
-curl_test=`curl "https://bing.com" -s|grep "www.bing.com"`  # baidu限速，因此换bing
+curl_test=`curl '1.1.1.1' -sS`
 if [ -n "${curl_test}" ]; then
-  echo "Connected!"
-else
-  # 获取有(user_ip、ac_ip、ac_name)参数以及host_name的登录url
-  login_url=`curl --interface $interface '1.1.1.1' -sS|grep '<a'|grep -Eo 'http[^"]*'`
-  echo ${login_url}
+  # 未登录时访问任意网址会得到一个登录跳转的html
+  # 登录跳转链接里有user_ip、ac_ip、ac_name参数
+  login_url=`echo ${curl_test}|grep '<a'|grep -Eo 'http[^"]*'|awk 'NR==1{print}'`
+  echo "login_url=${login_url}"
   if [ -n "${login_url}" ]; then
-    # 从login_url截取host_name，即认证服务器网址
-    # 学校的认证服务器一般在学校内网，所以只要不重启，host_name大概率是固定的裸内网ipv4
-    # 但是考虑到适用性，还是从login_url中截取
-    host_name=`echo ${login_url}|grep -Eo "/[^/?]+/"|grep -Eo "[^/]*"`  # host_name="10.10.244.11"
-    # host_name="p.njupt.edu.cn" # 这个能解析到服务器，但是仅能用于登出(应该
+    # 考虑到适用性，还是从login_url中截取host_name
+    host_name=`echo ${login_url}|grep -Eo "/[^/?]+/"|grep -Eo "[^/]*"`
+    # host_name="p.njupt.edu.cn" # 这个能解析到服务器，但是开了utun会影响解析
+    echo "server_ip=${host_name}"
 
     # 登录需要ac_name、ac_ip中任意一个
     ac_data=`echo ${login_url}|grep -Eo "wlanacname=[^&]*"`
-    echo ${ac_data} > ac_data.txt # 供njupt_logout.sh使用
+    echo ${ac_data} > ~/ac_data.txt # 供njupt_logout.sh使用
 
-    # 参考drcom连接教程修改而来
-    # 需要确认的有：请求地址、登录参数、用户、密码、路由器信息
-    curl --interface $interface \
-    "http://${host_name}:801/eportal/?c=ACSetting&a=Login" \
-      --data-urlencode "DDDDD=,0,${USER}${ISP}"\
-      --data-urlencode "upass=${PWD}"\
+    # 登录需要确认的有：请求地址、登录参数、用户、密码、路由器信息
+    curl "http://${host_name}:801/eportal/?c=ACSetting&a=Login" \
+      --data-urlencode "DDDDD=,0,${USER}${ISP}" \
+      --data-urlencode "upass=${PWD}" \
       --data "${ac_data}"
   fi
 
-  curl_test=`curl "https://bing.com" -s|grep "www.bing.com"`
+  curl_test=`curl "4.ipw.cn" -s|awk 'NR==1{print}'|grep -Eo "[0-9.]"`
   if [ -n "${curl_test}" ]; then
-    echo "Successful connection"
+    echo "Login successfully."
   else
-    echo "Connection error"
+    echo "Login failed!"
   fi
+else
+  echo "Connected!"
 fi
--- njupt_logout.sh
+++ njupt_logout.sh
@@ -6,8 +6,8 @@ unset http_proxy
 unset https_proxy
 unset ftp_proxy
 
-ac_data=`cat ac_data.txt`
+ac_data=`cat ~/ac_data.txt`
 host_name="p.njupt.edu.cn"
 
-curl "http://${host_name}:801/eportal/?c=ACSetting&a=Logout"\
+curl "http://${host_name}:801/eportal/?c=ACSetting&a=Logout" \
   --data "${ac_data}"
